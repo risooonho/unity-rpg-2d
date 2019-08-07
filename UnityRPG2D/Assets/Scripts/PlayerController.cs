6689f45bf4aac8a17c3695ebfd5a4e724f76b749
TrungNT <nttrung13091995@gmail.com>
2019-08-07 15:09:20 +0700

    player movement and area exit

--- UnityRPG2D/Assets/Scripts/PlayerController.cs
+++ UnityRPG2D/Assets/Scripts/PlayerController.cs
@@ -6,15 +6,24 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D theRB;
     public float moveSpeed;
+    public Animator myAnim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
         theRB.velocity = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")) * moveSpeed;
+        myAnim.SetFloat("moveX", theRB.velocity.x);
+        myAnim.SetFloat("moveY", theRB.velocity.y);
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 
+        || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
     }
 }
